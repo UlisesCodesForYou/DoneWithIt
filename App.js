d5fc46072a994e0da46efad7cbc3ff5ba197d28d
Ulises Orozco <ulisesorozco6@gmail.com>
2023-02-09 21:14:16 -0800

    Added functionality to the add goal button along with a real time rendered list.

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StyleSheet, TextInput, View, Button, Text } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  };
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your goals" />
-        <Button title="Add your goal" />
+      <View style={styles.goalContainer}>
+        <TextInput
+          placeholder="Your goals"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <>
+          {courseGoals.map((goal) => (
+            <Text key={goal}>{goal}</Text>
+          ))}
+        </>
       </View>
     </View>
   );
@@ -16,7 +37,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+
+  goalContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+
+  textInput: {
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 5,
   },
 });
 
